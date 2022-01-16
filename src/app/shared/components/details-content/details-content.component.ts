9b03aa8ee75d6d3973811e88d022651162377a6f
Andres G <agh92@hotmail.com>
2022-01-16 18:37:56 +0100

    add sharable components for the detail views

---
+++ src/app/shared/components/details-content/details-content.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-content',
+  templateUrl: './details-content.component.html',
+  styleUrls: ['./details-content.component.scss'],
+})
+export class DetailsContentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { CardGridComponent } from './components/card-grid/card-grid.component';
+import { DetailsContentComponent } from './components/details-content/details-content.component';
+import { DetailsHeaderComponent } from './components/details-header/details-header.component';
+import { DetailsSectionComponent } from './components/details-section/details-section.component';
 
 @NgModule({
-  declarations: [CardComponent, CardGridComponent],
+  declarations: [
+    CardComponent,
+    CardGridComponent,
+    DetailsContentComponent,
+    DetailsHeaderComponent,
+    DetailsSectionComponent,
+  ],
   imports: [CommonModule],
-  exports: [CardGridComponent],
+  exports: [
+    CardGridComponent,
+    DetailsContentComponent,
+    DetailsHeaderComponent,
+    DetailsSectionComponent,
+  ],
 })
 export class SharedModule {}
