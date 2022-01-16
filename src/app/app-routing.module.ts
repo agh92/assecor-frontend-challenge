e775eeb25d4d0e9a71e5a1e288825a1a66508294
Andres G <agh92@hotmail.com>
2022-01-16 13:59:37 +0100

    fix routing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'films',
-    loadChildren: () =>
-      import('./films/films.module').then((m) => m.FilmsModule),
+    loadChildren: () => import('./films/films.module').then((m) => m.FilmsModule),
   },
   {
     path: 'people',
-    loadChildren: () =>
-      import('./people/people.module').then((m) => m.PeopleModule),
+    loadChildren: () => import('./people/people.module').then((m) => m.PeopleModule),
   },
   {
     path: 'planets',
-    loadChildren: () =>
-      import('./planets/planets.module').then((m) => m.PlanetsModule),
+    loadChildren: () => import('./planets/planets.module').then((m) => m.PlanetsModule),
   },
   {
-    path: '',
-    redirectTo: '',
-    pathMatch: 'full',
+    path: '**',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/films/films-routing.module.ts
+++ src/app/films/films-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { FilmsComponent } from './films.component';
 import { FilmDetailsComponent } from './components/film-details/film-details.component';
 import { FilmResolver } from './resolvers/film.resolver';
+import { FilmsResolver } from './resolvers/films.resolver';
 
 const routes: Routes = [
-  { path: '', component: FilmsComponent, resolve: { films: FilmResolver } },
+  { path: '', component: FilmsComponent, resolve: { films: FilmsResolver } },
   {
     path: ':id',
     component: FilmDetailsComponent,
