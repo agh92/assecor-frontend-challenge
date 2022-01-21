48ab433cfae0867d4b9f069841a903864c55b1f7
Andres G <agh92@hotmail.com>
2022-01-21 16:26:09 +0100

    implement forms

--- src/app/core/components/form/form.component.ts
+++ src/app/core/components/form/form.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+export interface FormInput {
+  label: string;
+  type: 'input' | 'textArea' | 'dropdown';
+  /**
+   * For type input and textArea the expected extra is a string with the hint
+   * For dropdown type the expected extra is an array of strings with the options
+   */
+  extra: string | string[];
+}
+
+export interface ModalComponentOptions {
+  title: string;
+  /**
+   * The first dimension of the array represents the groups - a row inside the form
+   * the second dimension the children of a group - the items inside the row
+   */
+  groups: FormInput[][];
+}
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
+  @Input() options?: ModalComponentOptions;
 
-  constructor() { }
+  constructor(public modalRef: NgbActiveModal) {}
 
-  ngOnInit(): void {
+  isInput(input: FormInput) {
+    return input.type === 'input';
   }
 
+  isTextArea(input: FormInput) {
+    return input.type === 'textArea';
+  }
+
+  isDropDown(input: FormInput) {
+    return input.type === 'dropdown';
+  }
+
+  getDropDownExtra(input: FormInput) {
+    return input.extra as string[];
+  }
+
+  closeModal(event: Event) {
+    event.preventDefault();
+    this.modalRef.close();
+  }
 }
--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -1,6 +1,69 @@
 import { Injectable } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormComponent } from '../components/form/form.component';
+import { FormComponent, ModalComponentOptions } from '../components/form/form.component';
+
+const FILM_FORM: ModalComponentOptions = {
+  title: 'Add Film',
+  groups: [
+    [{ label: 'Title', type: 'input', extra: 'Add title' }],
+    [{ label: 'Director', type: 'input', extra: 'Add director' }],
+    [
+      {
+        label: 'Producer',
+        type: 'dropdown',
+        extra: ['Gary Kurtz', 'Rick McCallum', 'Howard G. Kazanjian', 'George Lucas'],
+      },
+    ],
+    [{ label: 'Release date', type: 'input', extra: 'YYYY' }],
+    [{ label: 'Description', type: 'textArea', extra: 'Description' }],
+  ],
+};
+
+const PERSON_FORM: ModalComponentOptions = {
+  title: 'Add Person',
+  groups: [
+    [{ label: 'Name', type: 'input', extra: 'Add name' }],
+    [
+      { label: 'Height', type: 'input', extra: 'Meters' },
+      { label: 'Weight', type: 'input', extra: 'Kg' },
+    ],
+    [
+      { label: 'Hair color', type: 'input', extra: 'Hair color' },
+      {
+        label: 'Eye color',
+        type: 'dropdown',
+        extra: ['red', 'blue', 'green', 'yellow', 'brown'],
+      },
+    ],
+    [
+      { label: 'Birth year', type: 'input', extra: 'YYYY' },
+      { label: 'Gender', type: 'dropdown', extra: ['female', 'male', 'n/a'] },
+    ],
+  ],
+};
+
+const PLANET_FORM: ModalComponentOptions = {
+  title: 'Add Planet',
+  groups: [
+    [{ label: 'Name', type: 'input', extra: 'Add name' }],
+    [
+      {
+        label: 'Type',
+        type: 'dropdown',
+        extra: ['desert', 'grasslands', 'mountains', ' tundra, ice caves'],
+      },
+    ],
+    [
+      {
+        label: 'Created by',
+        type: 'dropdown',
+        extra: ['George Lucas', 'Irvin Kershner', 'Richard Marquand'],
+      },
+    ],
+    [{ label: 'Genre', type: 'input', extra: 'science fiction' }],
+    [{ label: 'Race', type: 'textArea', extra: 'Races separated by semi-colon' }],
+  ],
+};
 
 @Injectable({
   providedIn: 'root',
@@ -9,18 +72,24 @@ export class ModalService {
   constructor(private ngbModalService: NgbModal) {}
 
   public openFilmForm() {
-    this.openModal();
+    this.openModal(FILM_FORM);
   }
 
   public openPersonForm() {
-    this.openModal();
+    this.openModal(PERSON_FORM);
   }
 
   public openPlanetForm() {
-    this.openModal();
+    this.openModal(PLANET_FORM);
   }
 
-  private openModal() {
-    this.ngbModalService.open(FormComponent);
+  private openModal(inputs: ModalComponentOptions) {
+    const modalRef = this.ngbModalService.open(FormComponent, {
+      backdropClass: 'blur-background',
+      centered: true,
+      backdrop: true,
+    });
+
+    (modalRef.componentInstance as FormComponent).options = inputs;
   }
 }
